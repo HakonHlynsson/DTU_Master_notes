726756c5e85132ecc62a87ab017404acb74d08ad
Hakon2003DTU <hakonhlyn@gmail.com>
2026-05-06 10:35:47 +0200

    update 6/5-2026

--- 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Lection_12(DAC)/L12_Execise_2/Core/Src/gpio.c
+++ 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Lection_12(DAC)/L12_Execise_2/Core/Src/gpio.c
@@ -21,34 +21,9 @@
 /* Includes ------------------------------------------------------------------*/
 #include "gpio.h"
 
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/*----------------------------------------------------------------------------*/
-/* Configure GPIO                                                             */
-/*----------------------------------------------------------------------------*/
-/* USER CODE BEGIN 1 */
-
-/* USER CODE END 1 */
-
-/** Configure pins as
-        * Analog
-        * Input
-        * Output
-        * EVENT_OUT
-        * EXTI
-*/
 void MX_GPIO_Init(void)
 {
-
-  /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-
 }
-
-/* USER CODE BEGIN 2 */
-
-/* USER CODE END 2 */
--- 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Lection_12(DAC)/L12_Execise_2/Core/Src/tim.c
+++ 1_Semester_(Spring_2026)/34373_Microcontroler_development_for_advanced_IoT_using_embedded_C/Lection_12(DAC)/L12_Execise_2/Core/Src/tim.c
@@ -20,25 +20,12 @@
 /* Includes ------------------------------------------------------------------*/
 #include "tim.h"
 
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
 TIM_HandleTypeDef htim6;
 
-/* TIM6 init function */
 void MX_TIM6_Init(void)
 {
-
-  /* USER CODE BEGIN TIM6_Init 0 */
-
-  /* USER CODE END TIM6_Init 0 */
-
   TIM_MasterConfigTypeDef sMasterConfig = {0};
 
-  /* USER CODE BEGIN TIM6_Init 1 */
-
-  /* USER CODE END TIM6_Init 1 */
   htim6.Instance = TIM6;
   htim6.Init.Prescaler = 63;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
@@ -54,41 +41,21 @@ void MX_TIM6_Init(void)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN TIM6_Init 2 */
-
-  /* USER CODE END TIM6_Init 2 */
-
 }
 
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
 {
-
   if(tim_baseHandle->Instance==TIM6)
   {
-  /* USER CODE BEGIN TIM6_MspInit 0 */
-
-  /* USER CODE END TIM6_MspInit 0 */
-    /* TIM6 clock enable */
     __HAL_RCC_TIM6_CLK_ENABLE();
-  /* USER CODE BEGIN TIM6_MspInit 1 */
-
-  /* USER CODE END TIM6_MspInit 1 */
   }
 }
 
 void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* tim_baseHandle)
 {
-
   if(tim_baseHandle->Instance==TIM6)
   {
-  /* USER CODE BEGIN TIM6_MspDeInit 0 */
-
-  /* USER CODE END TIM6_MspDeInit 0 */
-    /* Peripheral clock disable */
     __HAL_RCC_TIM6_CLK_DISABLE();
-  /* USER CODE BEGIN TIM6_MspDeInit 1 */
-
-  /* USER CODE END TIM6_MspDeInit 1 */
   }
 }
 
